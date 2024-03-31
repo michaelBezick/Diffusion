02f5a83f4b9f2bf1310439e02f6f3d01b3dd2cb3
Michael Tan Bezick <mbezick@gilbreth-fe01.rcac.purdue.edu>
2024-03-30 21:37:43 -0400

    Edits

--- VAE_Training.py
+++ VAE_Training.py
@@ -14,8 +14,8 @@ GOAL: Make a BVAE that compresses 32x32 space to 8x8. No energy correlation.
 """
 
 resume_from_checkpoint = False
-num_devices = 1
-num_nodes = 1
+num_devices = 2
+num_nodes = 2
 num_workers = 1
 accelerator = "gpu"
 batch_size = 100
