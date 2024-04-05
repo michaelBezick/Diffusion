523eebf250887e78ee224c1d796f9fd1a97db3d4
michaelBezick <mbezick@purdue.edu>
2024-04-05 12:33:12 -0400

    Edits

--- generate_dataset_Gilbreth.py
+++ generate_dataset_Gilbreth.py
@@ -3,19 +3,22 @@ import os
 
 from LDM_Classes import LDM, VAE, AttentionUNet
 
-experiment_name = "Experiment_3"
-experiment_notes = "Added saving FOM values to get correlation"
+experiment_name = "Experiment_4"
+experiment_notes = "Have sampling start at 1.4 mean and linearly increase to 1.8 during training. Variance held at 0.1"
 num_samples = 20_000
 batch_size = 1000
 mean = 1.7
 variance = 0.1
+variable_conditioning = True
 
 checkpoint_path_LDM = "./logs/LDM/version_1/checkpoints/epoch=3552-step=106590.ckpt"
 ############################################################
 
-FOM_values = variance * torch.randn(batch_size) + mean
-FOM_values = FOM_values.float()
-FOM_values = FOM_values.to("cuda")
+FOM_values = None
+if variable_conditioning == False:
+    FOM_values = variance * torch.randn(batch_size) + mean
+    FOM_values = FOM_values.float()
+    FOM_values = FOM_values.to("cuda")
 ddpm = AttentionUNet(UNet_channel=64, batch_size=batch_size).to("cuda")
 vae = VAE(h_dim=128, batch_size=batch_size).to("cuda")
 ldm = LDM.load_from_checkpoint(
@@ -31,7 +34,12 @@ ldm = LDM.load_from_checkpoint(
 ).to("cuda").eval()
 torch.set_float32_matmul_precision("high")
 
-dataset = ldm.create_dataset(num_samples=num_samples, FOM_values=FOM_values)
+dataset = None
+if variable_conditioning == False:
+    dataset = ldm.create_dataset(num_samples=num_samples, FOM_values=FOM_values)
+else:
+    dataset = ldm.create_dataset_variable_FOM(num_samples=num_samples, start_mean=1.4, end_mean=1.8, variance=0.1)
+
 dataset = torch.from_numpy(dataset)
 
 dir_path = "./Generated_Datasets/" + experiment_name + "/"
@@ -40,6 +48,7 @@ if not os.path.isdir(dir_path):
     os.mkdir(dir_path)
 
 torch.save(dataset, dir_path + "generated_dataset.pt")
-torch.save(FOM_values, dir_path + "FOM_values.pt")
+if variable_conditioning == False:
+    torch.save(FOM_values, dir_path + "FOM_values.pt")
 with open(dir_path + "Experiment_Notes.txt", "w") as file:
     file.write(experiment_notes)
