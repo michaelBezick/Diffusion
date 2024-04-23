1145a6bfcc5fee64b435259ae5a00b98f5adce05
michaelBezick <mbezick@purdue.edu>
2024-04-23 14:19:08 -0400

    Edits

--- generate_dataset_Gilbreth.py
+++ generate_dataset_Gilbreth.py
@@ -4,15 +4,15 @@ import numpy as np
 
 from LDM_Classes import LDM, VAE, AttentionUNet
 
-experiment_name = "Experiment_8"
-experiment_notes = "Same as 6,7 but changed saving so it works correct. mean 1.8, variance 0.1"
+experiment_name = "Experiment_9"
+experiment_notes = "Fixed conditioning mean 1.8, variance 0.1"
 num_samples = 20_000
 batch_size = 2000
 mean = 1.8
 variance = 0.1
 variable_conditioning = False
 
-checkpoint_path_LDM = "./logs/LDM/version_2/checkpoints/epoch=6999-step=210000.ckpt"
+checkpoint_path_LDM = "./logs/LDM/fixed_conditioning/checkpoints/epoch=3568-step=107070.ckpt"
 ############################################################
 
 FOM_values = None
