bccbf9a433628e5f34f60e8300cc9dce50f8e6da
Michael Bezick <mbezick@purdue.edu>
2025-01-16 09:46:30 -0500

    Edits

--- Ablation/generate_dataset.sh
+++ Ablation/generate_dataset.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
-module load anaconda/2020.11-py38
 cd $SLURM_SUBMIT_DIR
-python -m generate_dataset_Gilbreth.py
+module load anaconda/2020.11-py38
+export WORLD_SIZE=NUM_GPUS
+export NODE_RANK=RANK
+export MASTER_ADDR=MASTER_NODE_IP
+export MASTER_PORT=PORT
+srun ~/.local/bin/torchrun --nproc_per_node=1 generate_dataset_Gilbreth.py
+##!/bin/bash
+#cd $SLURM_SUBMIT_DIR
+#module load anaconda/2020.11-py38
+#python -m generate_dataset_Gilbreth.py
